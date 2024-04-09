3d5c3ef9d4dd10ad49bdd1146017de964b424528
Mahdi Hasnat Siyam <mahdi.siyam@chaldal.net>
2024-04-09 22:07:59 +0600

    Fix formatting in misc.cpp

--- Template/Miscellaneous.cpp
+++ Template/Miscellaneous.cpp
@@ -12,8 +12,7 @@ merge(a.begin(), a.end(),
     b.begin(), b.end(),
     back_inserter(c));
 #pragma GCC optimize("Ofast")
-#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,
-avx,avx2,fma")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimize("unroll-loops")
 #pragma GCC optimize("O3")
 
